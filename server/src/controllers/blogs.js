67c7d92f7519cab563d7aa9c709c81204c5393cf
mamoun kamal alshishani <82365853+MAMOUN-kamal-alshisani@users.noreply.github.com>
2023-05-17 18:53:08 +0300

    Update blogs.js

--- server/src/controllers/blogs.js
+++ server/src/controllers/blogs.js
@@ -132,7 +132,7 @@ export const createBlog = async (req, res) => {
       category: category,
       photo: photo,
       watched: watched,
-      UserId: UserId,
+//       UserId: UserId,
     });
     res.status(201).send(blogs);
   } catch (err) {
