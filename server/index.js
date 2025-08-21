f66c27b7853536fc5f41f5a4b643a2ed353e1b57
mamoun kamal alshishani <mamoun.alshishani@yahoo.com>
2025-08-21 16:03:54 +0300

    update cors

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ const server = express();
 
 /// middleware
 server.use(express.static("public"));
-server.use(cors({ origin: [process.env.DATABASE_URL,process.env.CLIENT_APP,process.env.FIREBASE_URL], credentials: true }));
+server.use(cors( {credentials: true }));
 server.use(cookieParser());
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
